75e12fe37872e19fc23a068ee2df06f8fdbe3126
nikolay <n.atanasow94@gmail.com>
2022-04-05 16:52:03 +0300

    Add cli commands
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

---
+++ src/cliHelper.js
@@ -0,0 +1,24 @@
+const net = require('net');
+
+module.exports = class CliHelper {
+  static getArgValue(args, needle, defaultValue = null) {
+    const item = args.find(arg => arg.indexOf('--' + needle) > -1);
+    if (!item) {
+      return defaultValue;
+    }
+
+    const chunks = item.split('=');
+    return chunks.length > 1 ? chunks[1].trim() : defaultValue;
+  }
+
+  static async waitForFiringUp(port) {
+    let isReady = false;
+    while (!isReady) {
+      net.createConnection(port).on('data', function () {
+        isReady = true;
+      });
+
+      await new Promise(r => setTimeout(r, 5000));
+    }
+  }
+}
