835176f44a2af802c232880e1d91784d38b8bf6f
Nathan Klick <nathan@swirldslabs.com>
2022-06-22 12:59:33 -0500

    Update postinstall.js and README.md
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

--- postinstall.js
+++ postinstall.js
@@ -1,4 +1,9 @@
 const shell = require('shelljs');
 
-shell.exec('sed -i \'s/PLATFORM_JAVA_OPTS/#PLATFORM_JAVA_OPTS/\' .env');
-shell.exec('docker-compose build');
+/*
+Neither of the below items are currently needed. The `PLATFORM_JAVA_OPTS` should not be commented out and the current
+docker compose definitions do not support inline builds.
+*/
+
+// shell.exec('sed -i \'s/PLATFORM_JAVA_OPTS/#PLATFORM_JAVA_OPTS/\' .env');
+// shell.exec('docker-compose build');
