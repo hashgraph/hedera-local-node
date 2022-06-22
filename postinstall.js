15a862c7956cfd6ce8b59234821ede106e03d037
Nathan Klick <nathan@swirldslabs.com>
2022-06-22 16:59:11 -0500

    Remove commented out code and related hooks
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

--- postinstall.js
+++
@@ -1,9 +0,0 @@
-const shell = require('shelljs');
-
-/*
-Neither of the below items are currently needed. The `PLATFORM_JAVA_OPTS` should not be commented out and the current
-docker compose definitions do not support inline builds.
-*/
-
-// shell.exec('sed -i \'s/PLATFORM_JAVA_OPTS/#PLATFORM_JAVA_OPTS/\' .env');
-// shell.exec('docker-compose build');
