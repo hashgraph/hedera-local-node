d9480cfa097ec5ad70ce1bdfb8b87efc722d1463
Ivo Yankov <ivo@devlabs.bg>
2022-08-16 10:25:49 +0300

    fix: resolve windows issue
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- cli.js
+++ cli.js
@@ -45,20 +45,27 @@ Available commands:
     }
   }
 
+  function getNullOutput() {
+    if (process.platform === 'win32') return 'nul';
+    return '/dev/null';
+  }
+
   async function start(commands) {
+    const nullOutput = getNullOutput();
+
     console.log('Starting the docker containers...');
     shell.cd(__dirname);
-    const output = shell.exec('docker-compose up -d 2>/dev/null');
+    const output = shell.exec(`docker-compose up -d 2>${nullOutput}`);
     if (output.code == 1) {
       const yaml = require('js-yaml');
       const fs = require('fs');
       const containersNames = Object.values(yaml.load(fs.readFileSync('docker-compose.yml')).services)
           .map(e => e.container_name)
           .join(' ');
-      shell.exec(`docker stop ${containersNames} 2>/dev/null 1>&2`);
-      shell.exec(`docker rm -f -v ${containersNames} 2>/dev/null 1>&2`);
+      shell.exec(`docker stop ${containersNames} 2>${nullOutput} 1>&2`);
+      shell.exec(`docker rm -f -v ${containersNames} 2>${nullOutput} 1>&2`);
       await stop();
-      shell.exec('docker-compose up -d 2>/dev/null');
+      shell.exec(`docker-compose up -d 2>${nullOutput}`);
     }
     await CliHelper.waitForFiringUp(5600);
     console.log('Starting the network...');
@@ -67,13 +74,15 @@ Available commands:
   }
 
   async function stop() {
+    const nullOutput = getNullOutput();
+
     console.log('Stopping the network...');
     shell.cd(__dirname);
     console.log('Stopping the docker containers...');
-    shell.exec('docker-compose down -v 2>/dev/null');
+    shell.exec(`docker-compose down -v 2>${nullOutput}`);
     console.log('Cleaning the volumes and temp files...');
-    shell.exec('rm -rf network-logs/* >/dev/null 2>&1');
-    shell.exec('docker network prune -f 2>/dev/null');
+    shell.exec(`rm -rf network-logs/* >${nullOutput} 2>&1`);
+    shell.exec(`docker network prune -f 2>${nullOutput}`);
   }
 
   process.exit();
