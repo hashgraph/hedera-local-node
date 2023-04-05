df6a60417175c3d4477942eb89b3ea6936e30093
Iliya Savov <isavov@users.noreply.github.com>
2023-04-05 16:24:10 +0300

    fix
    Signed-off-by: Iliya Savov <isavov@users.noreply.github.com>

--- compose-network/mirror-node/init.sh
+++ compose-network/mirror-node/init.sh
@@ -5,7 +5,7 @@ PGCONF="${PGCONF:-/var/lib/postgresql/data}"
 PGHBA="${PGCONF}/pg_hba.conf"
 DB_SPECIFIC_SQL="alter user :ownerUsername with createrole;"
 
-# TimescaleDB v2 schema no longer creates the REST API user, while v1 schema still does
+# v2 schema no longer creates the REST API user, while v1 schema still does
 if [[ "${SCHEMA_V2}" == "true" ]]; then
   DB_SPECIFIC_SQL="create user :restUsername with login password :'restPassword' in role readonly;"
 fi
