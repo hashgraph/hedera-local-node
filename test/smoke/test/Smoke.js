e6b19212dc37d015c3d0d94b8e09d16c3b47e946
Ivo Yankov <ivo@devlabs.bg>
2023-06-30 15:14:40 +0300

    feat: add smoke test for ws
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- test/smoke/test/Smoke.js
+++ test/smoke/test/Smoke.js
@@ -3,6 +3,7 @@ const hre = require("hardhat");
 
 describe("Smoke Tests", async function () {
   let walletSender, walletReceiver;
+  const WEBSOCKET_URL = 'http://localhost:8546';
 
   before(async function () {
     const signers = await hre.ethers.getSigners();
@@ -10,22 +11,62 @@ describe("Smoke Tests", async function () {
     walletReceiver = signers[1];
   });
 
-  it("Should perform a crypto transfer transaction", async function() {
-    const amount = 100_000_000_000;
-    const amountbn = hre.ethers.BigNumber.from(amount);
-    const senderBalanceBefore = await walletSender.getBalance();
-    const receiverBalanceBefore = await walletReceiver.getBalance();
+  describe("Relay", async function() {
+    it("Should perform a crypto transfer transaction", async function() {
+      const amount = 100_000_000_000;
+      const amountbn = hre.ethers.BigNumber.from(amount);
+      const senderBalanceBefore = await walletSender.getBalance();
+      const receiverBalanceBefore = await walletReceiver.getBalance();
 
-    await walletSender.sendTransaction({
-      to: walletReceiver.address,
-      value: amount
+      await walletSender.sendTransaction({
+        to: walletReceiver.address,
+        value: amount
+      });
+
+      const senderBalanceAfter = await walletSender.getBalance();
+      const receiverBalanceAfter = await walletReceiver.getBalance();
+
+      expect(senderBalanceAfter.toString()).to.not.eq(senderBalanceBefore.toString());
+      expect(senderBalanceAfter.lt(senderBalanceBefore.sub(amountbn))).to.eq(true); // account for used gas
+      expect(receiverBalanceAfter).to.greaterThan(receiverBalanceBefore);
+    });
+  });
+
+  describe("Websocket server", async function() {
+    let logger, provider;
+
+    before(async function() {
+      const Logger = await hre.ethers.getContractFactory("Logger");
+      logger = await Logger.deploy({gasLimit: 1000000});
+      logger = await logger.deployed();
+
+      provider = await new hre.ethers.providers.WebSocketProvider(WEBSOCKET_URL);
+
+      provider._websocket.on('close', (code, message) => {
+        console.error(`Websocket closed: ${code}`);
+        console.error(message);
+      })
+
     });
 
-    const senderBalanceAfter = await walletSender.getBalance();
-    const receiverBalanceAfter = await walletReceiver.getBalance();
+    it("Should receive events for subscribed contract", async function() {
+      const logHandled = new Promise(resolve => {
+        provider.on({address: logger.address}, (event) => {
+          expect(event).to.exist;
+          expect(event.transactionHash).to.exist;
+          expect(event.blockHash).to.exist;
+          resolve();
+        });
+      });
 
-    expect(senderBalanceAfter.toString()).to.not.eq(senderBalanceBefore.toString());
-    expect(senderBalanceAfter.lt(senderBalanceBefore.sub(amountbn))).to.eq(true); // account for used gas
-    expect(receiverBalanceAfter).to.greaterThan(receiverBalanceBefore);
+      const tx = await logger.logEvent(1, 2, {gasLimit: 100000});
+      const rec = await tx.wait();
+
+      expect(rec).to.exist;
+      expect(rec.transactionHash).to.exist;
+
+      await logHandled;
+
+    }).timeout(10000);
   });
 });
