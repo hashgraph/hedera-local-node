6234ea85aed85bdf453e75ae55f4f085b085a4ca
Stefan Stefanov <stefan.stefanooov@gmail.com>
2024-01-03 17:06:40 +0200

    Comments for src/state AccountCreation
    Signed-off-by: Stefan Stefanov <stefan.stefanooov@gmail.com>

--- src/state/AccountCreationState.ts
+++ src/state/AccountCreationState.ts
@@ -39,21 +39,55 @@ import {
 import { EVM_ADDRESSES_BLOCKLIST_FILE_RELATIVE_PATH } from '../constants';
 import local from '../configuration/local.json';
 
+/**
+ * Represents the state of account creation.
+ * This class is responsible for initializing the AccountCreationState object.
+ * @implements {IState}
+ */
 export class AccountCreationState implements IState{
+    /**
+     * The logger used for logging account creation state information.
+     */
     private logger: LoggerService;
 
+    /**
+     * The CLI service used for account creation.
+     */
     private cliService: CLIService;
 
+    /**
+     * The client service used for account creation.
+     */
     private clientService: ClientService;
 
+    /**
+     * The observer for the account creation state.
+     */
     private observer: IOBserver | undefined;
 
+    /**
+     * The name of the state.
+     */
     private stateName: string;
 
+    /**
+     * Indicates whether the node is being started up.
+     */
     private nodeStartup: boolean;
 
+    /**
+     * The count of blocklisted accounts.
+     * This is used to offset the account numbers.
+     * This is only used when the node is being started up.
+     * @private
+     * Thre default value is 0.
+     */
     private blocklistedAccountsCount: number = 0;
     
+    /**
+     * Represents the state of account creation.
+     * This class is responsible for initializing the AccountCreationState object.
+     */
     constructor() {
         this.stateName = AccountCreationState.name;
         this.logger = ServiceLocator.Current.get<LoggerService>(LoggerService.name);
@@ -63,10 +97,25 @@ export class AccountCreationState implements IState{
         this.logger.trace('Account Creaton State Initialized!', this.stateName);
     }
 
+    /**
+     * Subscribes an observer to receive updates from the AccountCreationState.
+     * @param {IOBserver} observer The observer to subscribe.
+     */
     public subscribe(observer: IOBserver): void {
         this.observer = observer;
     }
 
+    /**
+     * Starts the account creation state.
+     * 
+     * This method retrieves the current arguments, checks if blocklisting is enabled, and if so, gets the count of blocklisted accounts.
+     * It logs the start of the account creation state, retrieves the balance and number of accounts from the arguments, and sets the node startup.
+     * If the mode is asynchronous, it generates accounts asynchronously, otherwise, it generates ECDSA, alias ECDSA, and ED25519 accounts.
+     * Finally, it updates the observer with the finish event type.
+     * 
+     * @returns {Promise<void>} A Promise that resolves when the state is started.
+     * @emits {EventType.Finish} When the state is finished.
+     */
     public async onStart(): Promise<void> {
         const currentArgv = this.cliService.getCurrentArgv();
         const { async, blocklisting } = currentArgv;
@@ -95,7 +144,16 @@ export class AccountCreationState implements IState{
         this.observer!.update(EventType.Finish);
     }
 
-    private async generateAsync(balance: number, accountNum: number) {
+    /**
+     * Generates accounts asynchronously.
+     * 
+     * This method generates ECDSA, alias ECDSA, and ED25519 accounts asynchronously, and logs the accounts.
+     * 
+     * @param {number} balance The balance of the accounts.
+     * @param {number} accountNum The number of accounts to generate.
+     * @returns {Promise<void>} A Promise that resolves when the accounts are generated.
+     */
+    private async generateAsync(balance: number, accountNum: number): Promise<void> {
         Promise.all([
             await this.generateECDSA(true, balance, accountNum),
             await this.generateAliasECDSA(true, balance, accountNum),
@@ -141,7 +199,22 @@ export class AccountCreationState implements IState{
             this.logAccountDivider();
         });
     }
-
+    
+    /**
+     * Generates ECDSA accounts.
+     * 
+     * This method initializes the ECDSA account number counter and an array to hold the accounts.
+     * It then generates the specified number of accounts.
+     * If the node is in startup mode, it uses the private keys from the ECDSA private keys array to create the wallets, otherwise, it generates new ECDSA private keys.
+     * If the mode is asynchronous, it creates the accounts asynchronously and adds them to the accounts array, otherwise, it creates the accounts synchronously.
+     * Finally, if the mode is not asynchronous, it logs a divider, otherwise, it returns a promise that resolves when all the accounts have been created.
+     * 
+     * @private
+     * @param {boolean} async - Whether the mode is asynchronous.
+     * @param {number} balance - The balance for the accounts.
+     * @param {number} accountNum - The number of accounts to generate.
+     * @returns {Promise<void | Wallet[]>} - A promise that resolves when all the accounts have been created if the mode is asynchronous, otherwise void.
+     */
     private async generateECDSA(async: boolean, balance: number, accountNum: number) {
         let ecdsaAccountNumCounter = 1002 + this.blocklistedAccountsCount;
         const accounts = [];
@@ -188,6 +261,21 @@ export class AccountCreationState implements IState{
         }
     }
 
+    /**
+     * Generates alias ECDSA accounts.
+     * 
+     * This method initializes the alias account number counter and an array to hold the accounts.
+     * It then generates the specified number of accounts.
+     * If the node is in startup mode and the private key for the alias ECDSA account exists, it uses the private key to create the wallet, otherwise, it creates a random wallet.
+     * If the mode is asynchronous, it creates the alias accounts asynchronously and adds them to the accounts array, otherwise, it creates the alias accounts synchronously and logs them.
+     * Finally, if the mode is asynchronous, it returns a promise that resolves when all the alias accounts have been created, otherwise, it logs a divider.
+     * 
+     * @private
+     * @param {boolean} async - Whether the mode is asynchronous.
+     * @param {number} balance - The balance for the accounts.
+     * @param {number} accountNum - The number of accounts to generate.
+     * @returns {Promise<void | Account[]>} - A promise that resolves when all the alias accounts have been created if the mode is asynchronous, otherwise void.
+     */
     private async generateAliasECDSA(async: boolean, balance: number, accountNum: number) {
         let aliasedAccountNumCounter = 1012 + this.blocklistedAccountsCount;
         const accounts = [];
@@ -230,6 +318,21 @@ export class AccountCreationState implements IState{
         this.logAliasAccountDivider();
     }
 
+    /**
+     * Generates ED25519 accounts.
+     * 
+     * This method initializes the ED25519 account number counter and an array to hold the accounts.
+     * It then generates the specified number of accounts.
+     * If the node is in startup mode, it uses the private keys from the ED25519 private keys array to create the wallets, otherwise, it generates new ED25519 private keys.
+     * If the mode is asynchronous, it creates the accounts asynchronously and adds them to the accounts array, otherwise, it creates the accounts synchronously.
+     * Finally, if the mode is not asynchronous, it logs a divider, otherwise, it returns a promise that resolves when all the accounts have been created.
+     * 
+     * @private
+     * @param {boolean} async - Whether the mode is asynchronous.
+     * @param {number} balance - The balance for the accounts.
+     * @param {number} accountNum - The number of accounts to generate.
+     * @returns {Promise<void | Wallet[]>} - A promise that resolves when all the accounts have been created if the mode is asynchronous, otherwise void.
+     */
     private async generateED25519(async: boolean, balance: number, accountNum: number) {
         let edAccountNumCounter = 1022 + this.blocklistedAccountsCount;
         const accounts = [];
@@ -279,6 +382,21 @@ export class AccountCreationState implements IState{
         }
     }
 
+    /**
+     * Creates an account asynchronously.
+     * 
+     * This method retrieves the client, creates an account creation transaction with the public key from the wallet and the initial balance, and executes the transaction.
+     * It initializes the account ID with a default value.
+     * If the node is not in startup mode, it gets the receipt of the transaction and updates the account ID with the ID from the receipt.
+     * Finally, it returns an object with the account ID, the wallet, and the balance.
+     * 
+     * @private
+     * @param {number} accountNum - The account number.
+     * @param {number} balance - The balance for the account.
+     * @param {Wallet} wallet - The wallet for the account.
+     * @param {PrivateKey} privateKey - The private key for the account.
+     * @returns {Promise<Account>} - A promise that resolves with the created account.
+     */
     private async createAccountAsync (accountNum: number, balance: number, wallet: Wallet, privateKey: PrivateKey): Promise<Account> {
         const client = this.clientService.getClient();
         const tx = await new AccountCreateTransaction()
@@ -299,6 +417,21 @@ export class AccountCreationState implements IState{
         };
     }
 
+    /**
+     * Creates an account synchronously.
+     * 
+     * This method retrieves the client, creates an account creation transaction with the public key from the wallet and the initial balance, and executes the transaction.
+     * It initializes the account ID with a default value.
+     * If the node is not in startup mode, it gets the receipt of the transaction and updates the account ID with the ID from the receipt.
+     * Finally, it logs the account ID, the balance, and the raw string representation of the private key.
+     * 
+     * @private
+     * @param {number} accountNum - The account number.
+     * @param {number} balance - The balance for the account.
+     * @param {Wallet} wallet - The wallet for the account.
+     * @param {PrivateKey} privateKey - The private key for the account.
+     * @returns {Promise<void>} - A promise that resolves when the account has been created and logged.
+     */
     private async createAccount (accountNum: number, balance: number, wallet: Wallet, privateKey: PrivateKey) {
         const client = this.clientService.getClient();
         const tx = await new AccountCreateTransaction()
@@ -319,6 +452,23 @@ export class AccountCreationState implements IState{
         );
     }
     
+    /**
+     * Creates an alias account.
+     * 
+     * This method retrieves the client, creates a public key from the compressed public key of the wallet's signing key, and converts it to an account ID.
+     * It creates a transfer transaction that transfers the specified balance from the account ID to the account with ID '0.0.2' and executes the transaction.
+     * It initializes the account number with a default value.
+     * If the node is not in startup mode or the mode is asynchronous, it gets the receipt of the transaction and retrieves the account info for the account with the EVM address of the wallet.
+     * It updates the account number with the ID from the account info.
+     * Finally, it returns an object with the account ID, the wallet, and the balance.
+     * 
+     * @private
+     * @param {boolean} async - Whether the mode is asynchronous.
+     * @param {number} aliasedAccountNumCounter - The alias account number counter.
+     * @param {number} balance - The balance for the account.
+     * @param {ethers.Wallet} wallet - The wallet for the account.
+     * @returns {Promise<Account>} - A promise that resolves with the created alias account.
+     */
     private async createAliasAccount (async: boolean, aliasedAccountNumCounter: number, balance: number, wallet: ethers.Wallet): Promise<Account> {
         const client = this.clientService.getClient();
         const accountId = PublicKey.fromString(
@@ -340,11 +490,30 @@ export class AccountCreationState implements IState{
         return { accountId: accountNum, wallet, balance };
     }
 
+    /**
+     * Retrieves the blocklist file name.
+     * 
+     * This method searches the properties of the node configuration for the property with the key 'accounts.blocklist.path' and returns its value.
+     * 
+     * @private
+     * @returns {string} - The blocklist file name.
+     */
     private blockListFileName(): string {
         return local.nodeConfiguration.properties
             .find((prop) => prop.key === 'accounts.blocklist.path')?.value as string
     }
 
+    /**
+     * Retrieves the count of blocklisted accounts.
+     * 
+     * This method creates a new promise that resolves with the count of blocklisted accounts.
+     * It initializes the count to 0 and constructs the file path to the blocklist file.
+     * It creates a read stream from the file, pipes it through a CSV parser, and increments the count for each data event.
+     * When the end event is emitted, it resolves the promise with the count.
+     * 
+     * @private
+     * @returns {Promise<number>} - A promise that resolves with the count of blocklisted accounts.
+     */
     private async getBlocklistedAccountsCount (): Promise<number> {
         return new Promise((resolve) => {
             let count = 0;
@@ -364,11 +533,31 @@ export class AccountCreationState implements IState{
         })
     }
 
+    /**
+     * Logs an account.
+     * 
+     * This method logs the account ID, the private key, and the balance of an account, along with the state name.
+     * 
+     * @private
+     * @param {string} accountId - The account ID.
+     * @param {Hbar} balance - The balance of the account.
+     * @param {string} privateKey - The private key of the account.
+     */
     private logAccount (accountId: string, balance: Hbar, privateKey: string) {
         this.logger.info(`| ${accountId} - ${privateKey} - ${balance} |`, this.stateName);
     }
 
-    private logAliasAccount (accountId: string, balance: number, wallet: ethers.Wallet) {
+    /**
+     * Logs an alias account.
+     * 
+     * This method logs the account ID, the wallet address, the private key of the wallet's signing key, and the balance of an alias account, along with the state name.
+     * 
+     * @private
+     * @param {string} accountId - The account ID.
+     * @param {number} balance - The balance of the account.
+     * @param {ethers.Wallet} wallet - The wallet of the account.
+     */
+    private logAliasAccount (accountId: string, balance: number, wallet: ethers.Wallet): void {
         this.logger.info(
             `| ${accountId} - ${wallet.address} - ${
             wallet.signingKey.privateKey
@@ -377,6 +566,14 @@ export class AccountCreationState implements IState{
         );
     }
 
+    /**
+     * Logs the title of an account.
+     * 
+     * This method logs a divider, the title of the account list with the account type, another divider, the headers for the account ID, private key, and balance, and a final divider.
+     * 
+     * @private
+     * @param {string} accountType - The type of the account.
+     */
     private logAccountTitle (accountType: string) {
         this.logAccountDivider();
         this.logger.info(
@@ -391,6 +588,13 @@ export class AccountCreationState implements IState{
         this.logAccountDivider();
     }
 
+    /**
+     * Logs the title of an alias account.
+     * 
+     * This method logs a divider, the title of the alias account list, another divider, the headers for the account ID, public address, private key, and balance, and a final divider.
+     * 
+     * @private
+     */
     private logAliasAccountTitle () {
         this.logAliasAccountDivider();
         this.logger.info(
@@ -405,6 +609,13 @@ export class AccountCreationState implements IState{
         this.logAliasAccountDivider();
     }
 
+    /**
+     * Logs a divider for an account.
+     * 
+     * This method logs a divider line, along with the state name.
+     * 
+     * @private
+     */
     private logAccountDivider () {
         this.logger.info(
             '|-----------------------------------------------------------------------------------------|',
@@ -412,6 +623,13 @@ export class AccountCreationState implements IState{
         );
     }
 
+    /**
+     * Logs a divider for an alias account.
+     * 
+     * This method logs a divider line, along with the state name.
+     * 
+     * @private
+     */
     private logAliasAccountDivider () {
         this.logger.info(
             '|--------------------------------------------------------------------------------------------------------------------------------------|',
