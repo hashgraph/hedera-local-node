ea63b80cb1ca170f667dd71056f672b8da1fae1a
Konstantina Blazhukova <konstantina.blajukova@gmail.com>
2024-01-12 14:26:43 +0200

    Tries to fix 2nd test
    Signed-off-by: Konstantina Blazhukova <konstantina.blajukova@gmail.com>

--- test/StateController.spec.ts
+++ test/StateController.spec.ts
@@ -1,4 +1,4 @@
-import { expect } from 'chai';
+import { expect, assert } from 'chai';
 import sinon from 'sinon';
 import { StateController } from '../src/controller/StateController';
 import { EventType } from '../src/types/EventType';
@@ -17,7 +17,24 @@ import { StateConfiguration } from '../src/types/StateConfiguration';
 describe('StateController', () => {
   let stateController: StateController;
   let serviceLocatorStub: sinon.SinonStub;
-  let stateDataStub: sinon.SinonStubbedInstance<StateData>;
+  let stateDataStub: sinon.SinonStub;
+  let cleanUpStateStub: sinon.SinonStubbedInstance<CleanUpState>;
+  let cleanUpStateStub2 : sinon.SinonStubbedInstance<CleanUpState>;
+  let getStartConfigurationStub: sinon.SinonStub;
+  //let serviceLocatorStub;
+  // const stubbedServiceLocator = sinon.createStubInstance(ServiceLocator);
+  // const loggerServiceStub = sinon.createStubInstance(LoggerService);
+  //
+  // stubbedServiceLocator.get.callsFake(function() {
+  //   return loggerServiceStub;
+  // });
+  //
+  // sinon.stub(ServiceLocator, 'Current').callsFake(function () {
+  //   return stubbedServiceLocator;
+  // });
+  //
+  // stateController = new StateController('start');
+  // process.exit(1);
 
   beforeEach(() => {
     // Create a stub for the ServiceLocator
@@ -25,7 +42,9 @@ describe('StateController', () => {
     const stubbedStartState = sinon.createStubInstance(StartState);
     const stubbedNetworkPrepState = sinon.createStubInstance(NetworkPrepState);
     const stubbedAccountCreationState = sinon.createStubInstance(AccountCreationState);
-    const stubbedCleanUpState = sinon.createStubInstance(CleanUpState);
+    cleanUpStateStub = sinon.createStubInstance(CleanUpState);
+    cleanUpStateStub2 = sinon.createStubInstance(CleanUpState);
+    cleanUpStateStub2.onStart.resolves();
     const stubbedAttachState = sinon.createStubInstance(AttachState);
     const loggerServiceStub = sinon.createStubInstance(LoggerService);
     const stubbedConfiguration: StateConfiguration = {
@@ -35,49 +54,63 @@ describe('StateController', () => {
           // Add more stubbed states as needed
       ]
     };
-    // const stateDataStub = sinon.createStubInstance(StateData,{
-    //   getSelectedStateConfiguration: stubbedConfiguration
-    // });
-    //serviceLocatorStub = sinon.stub(ServiceLocator.Current, 'get').returns(new LoggerService(0));
-    // Stub ServiceLocator
-    sinon.stub(ServiceLocator.Current, 'get').returns(loggerServiceStub);
-    // Initialize the StateController
-    //stateController = new StateController('start');
-    // stateDataStub.getSelectedStateConfiguration.restore();
-    // sinon.stub(stateDataStub, 'getSelectedStateConfiguration').returns(stubbedConfiguration);
-    // Stub the creation of StateData instance
-    //sinon.stub(StateData.prototype, 'getSelectedStateConfiguration').returns(stateDataStub);
-    // Initialize the StateController
-    stateDataStub = sinon.createStubInstance(StateData, {
-      getSelectedStateConfiguration: stubbedConfiguration
+
+    serviceLocatorStub = sinon.stub(ServiceLocator.Current, 'get').returns(loggerServiceStub);
+
+    //stateDataStub = sinon.stub(StateData.prototype, <any>'getSelectedStateConfiguration').returns(undefined);
+
+    getStartConfigurationStub = sinon.stub(StateData.prototype, <any>"getStartConfiguration").returns({
+        'stateMachineName' : 'start',
+        'states' : [
+          stubbedInitState,
+          stubbedStartState,
+          stubbedNetworkPrepState,
+          stubbedAccountCreationState,
+          cleanUpStateStub,
+          stubbedAttachState
+        ]
     });
     stateController = new StateController('start');
   });
 
   afterEach(() => {
     // Restore the original ServiceLocator after each test
+    cleanUpStateStub.onStart.restore();
     serviceLocatorStub.restore();
+    getStartConfigurationStub.restore();
+    //stateDataStub.getSelectedStateConfiguration.restore();
+    getStartConfigurationStub.restore();
   });
 
-  it.only('should transition to next state on Finish event', async () => {
-    //const stateController = new StateController('start');
+  it('should transition to next state on Finish event', async () => {
+    // Starting the state machine
     await stateController.startStateMachine();
+
+    // Sending two finish events
     await stateController.update(EventType.Finish);
-    console.log(stateController['currStateNum']);
-    expect(stateController['currStateNum']).to.equal(1);
+    await stateController.update(EventType.Finish);
+
+    expect(stateController['currStateNum']).to.equal(2);
   });
 
-  it('should handle UnknownError or UnresolvableError event correctly', async () => {
-    const stateController = new StateController('exampleState');
-
-    await stateController.update(EventType.UnknownError);
-  });
 
   it('should handle other events correctly', async () => {
-    const stateController = new StateController('exampleState');
+    // Arrange
+    // Stub process.exit
+    const processExitStub = sinon.stub(process, 'exit');
+    // Act
+    // await stateController.update(EventType.UnknownError);
+    await stateController.update(EventType.UnresolvableError);
 
-    // Mocking a custom event
-    const customEvent = 'CustomEvent';
-    await stateController.update(EventType.Finish);
+    // Assert
+    assert.isTrue(cleanUpStateStub2.onStart.calledOnce);
+    assert.isTrue(processExitStub.calledWith(1));
+    //assert.isTrue(0);
+    // assert.isTrue(unresolvableErrorStub.calledOnce);
+    // assert.isTrue(processExitStub.calledWith(1));
+
+    // Restore the stubs for the next test
+    // unknownErrorStub.restore();
+    // unresolvableErrorStub.restore();
   });
 });
