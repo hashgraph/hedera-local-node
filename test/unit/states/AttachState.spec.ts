dea068f537892d25b134f1bf0d3f5e5d7bdaf005
Konstantina Blazhukova <konstantina.blajukova@gmail.com>
2024-02-06 10:07:05 +0200

    Makes use of the sandbox fro mtest bed
    Signed-off-by: Konstantina Blazhukova <konstantina.blajukova@gmail.com>

--- test/unit/states/AttachState.spec.ts
+++ test/unit/states/AttachState.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import sinon, { SinonStubbedInstance } from 'sinon';
+import sinon, { SinonSandbox, SinonStubbedInstance } from 'sinon';
 import { AttachState } from '../../../src/state/AttachState';
 import { IOBserver } from '../../../src/controller/IObserver';
 import { CLIService } from '../../../src/services/CLIService';
@@ -12,10 +12,12 @@ describe('AttachState', () => {
   let attachState: AttachState,
       loggerService: SinonStubbedInstance<LoggerService>,
       dockerService: SinonStubbedInstance<DockerService>,
-      cliService: SinonStubbedInstance<CLIService>;
+      cliService: SinonStubbedInstance<CLIService>,
+      testSandbox: SinonSandbox;
 
-  beforeEach(() => {
+  before(() => {
     const {
+        sandbox,
         loggerServiceStub,
         dockerServiceStub,
         cliServiceStub
@@ -26,10 +28,15 @@ describe('AttachState', () => {
     dockerService = dockerServiceStub
     loggerService = loggerServiceStub
     cliService = cliServiceStub
+    testSandbox = sandbox
 
     // Create an instance of AttachState
     attachState = new AttachState();
   });
+
+  after(() => {
+    testSandbox.resetHistory();
+  })
 
   describe('subscribe', () => {
     it('should set the observer', () => {
@@ -50,8 +57,8 @@ describe('AttachState', () => {
         startup: false,
         detached: false
       } as any);
-      const attachContainerLogsStub = sinon.stub(AttachState.prototype, <any>'attachContainerLogs');
-      const iterations = sinon.stub(AttachState.prototype, <any>'loopIterations');
+      const attachContainerLogsStub = testSandbox.stub(AttachState.prototype, <any>'attachContainerLogs');
+      const iterations = testSandbox.stub(AttachState.prototype, <any>'loopIterations');
       iterations.returns(2);
 
       await attachState.onStart();
@@ -60,14 +67,14 @@ describe('AttachState', () => {
       expect(attachContainerLogsStub.firstCall.calledWithExactly("network-node")).to.be.true;
       expect(attachContainerLogsStub.secondCall.calledWithExactly("mirror-node-rest")).to.be.true;
       expect(attachContainerLogsStub.thirdCall.calledWithExactly("json-rpc-relay")).to.be.true;
-      sinon.assert.calledTwice(loggerService.updateStatusBoard);
+      testSandbox.assert.calledTwice(loggerService.updateStatusBoard);
 
       attachContainerLogsStub.restore();
       iterations.restore();
     }).timeout(20000);
 
     it('should not call attachContainerLogs when detached', async () => {
-      (attachState as any).observer = { update: sinon.stub()};
+      (attachState as any).observer = { update: testSandbox.stub()};
       cliService.getCurrentArgv.returns({
         async: false,
         blocklisting: false,
@@ -76,13 +83,13 @@ describe('AttachState', () => {
         startup: false,
         detached: true
       } as any);
-      const attachContainerLogsStub = sinon.stub(AttachState.prototype, <any>'attachContainerLogs');
-      const iterations = sinon.stub(AttachState.prototype, <any>'loopIterations');
+      const attachContainerLogsStub = testSandbox.stub(AttachState.prototype, <any>'attachContainerLogs');
+      const iterations = testSandbox.stub(AttachState.prototype, <any>'loopIterations');
       iterations.returns(2);
 
       await attachState.onStart();
 
-      sinon.assert.calledOnceWithExactly(attachState.observer?.update, EventType.Finish);
+      testSandbox.assert.calledOnceWithExactly(attachState.observer?.update, EventType.Finish);
       attachContainerLogsStub.restore();
       iterations.restore();
     }).timeout(20000);
@@ -98,14 +105,14 @@ describe('AttachState', () => {
         startup: false,
         detached: false
       } as any);
-      const logsSpy = sinon.spy();
-      const demuxSpy = sinon.spy();
+      const logsSpy = testSandbox.spy();
+      const demuxSpy = testSandbox.spy();
       dockerService.getContainer.resolves({
         logs: logsSpy,
         modem: { demuxStream: demuxSpy },
       } as any);
-      const attachContainerLogsStub = sinon.stub(AttachState.prototype, <any>'attachContainerLogs');
-      const iterations = sinon.stub(AttachState.prototype, <any>'loopIterations');
+      const attachContainerLogsStub = testSandbox.stub(AttachState.prototype, <any>'attachContainerLogs');
+      const iterations = testSandbox.stub(AttachState.prototype, <any>'loopIterations');
       iterations.returns(2);
       attachContainerLogsStub.withArgs("mirror-node-rest").resolves();
       attachContainerLogsStub.withArgs("json-rpc-relay").resolves();
@@ -113,11 +120,11 @@ describe('AttachState', () => {
 
       await attachState.onStart();
 
-      sinon.assert.calledOnce(dockerService.getContainer);
+      testSandbox.assert.calledOnce(dockerService.getContainer);
       const spy1 = dockerService.getContainer.getCall(0);
 
-      sinon.assert.calledWithExactly(spy1, "network-node");
-      sinon.assert.calledOnce(logsSpy);
+      testSandbox.assert.calledWithExactly(spy1, "network-node");
+      testSandbox.assert.calledOnce(logsSpy);
     }).timeout(20000);
   });
 });
